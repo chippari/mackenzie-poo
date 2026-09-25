15223235668ddf7946f5366ca47e8dfd4575cf8b
Fabio Chippari <167870987+chippari@users.noreply.github.com>
2026-09-25 21:20:35 +0000

    fix: changed the name of the examples

--- Tarefa6/Main.java
+++ Tarefa6/Main.java
@@ -2,26 +2,26 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        FastFood burger = new FastFood("Burger Point", "Hamburguer");
-        FineDining maison = new FineDining("Maison Lumiere", 1);
-        FineDining terroir = new FineDining("Terroir", 3);
+        FastFood burger = new FastFood("Burger King", "Hamburguer");
+        FineDining murakami = new FineDining("Murakami", 1);
+        FineDining evvai = new FineDining("Evvai", 3);
 
         burger.adicionarAvaliacao(new Avaliacao("10/09/2026", 4, "Lanche muito bom"));
         burger.adicionarAvaliacao(new Avaliacao("12/09/2026", 3, "Demorou um pouco"));
         burger.adicionarAvaliacao(new Avaliacao("15/09/2026", 5, "Melhor da regiao"));
 
-        maison.adicionarAvaliacao(new Avaliacao("05/09/2026", 4, "Pratos elegantes"));
-        maison.adicionarAvaliacao(new Avaliacao("08/09/2026", 5, "Experiencia incrivel"));
-        maison.adicionarAvaliacao(new Avaliacao("20/09/2026", 3, "Porcoes pequenas"));
+        murakami.adicionarAvaliacao(new Avaliacao("05/09/2026", 4, "Pratos elegantes"));
+        murakami.adicionarAvaliacao(new Avaliacao("08/09/2026", 5, "Experiencia incrivel"));
+        murakami.adicionarAvaliacao(new Avaliacao("20/09/2026", 3, "Porcoes pequenas"));
 
-        terroir.adicionarAvaliacao(new Avaliacao("02/09/2026", 4, "Carta de vinhos otima"));
-        terroir.adicionarAvaliacao(new Avaliacao("11/09/2026", 4, "Atendimento impecavel"));
-        terroir.adicionarAvaliacao(new Avaliacao("18/09/2026", 5, "Inesquecivel"));
+        evvai.adicionarAvaliacao(new Avaliacao("02/09/2026", 4, "Carta de vinhos otima"));
+        evvai.adicionarAvaliacao(new Avaliacao("11/09/2026", 4, "Atendimento impecavel"));
+        evvai.adicionarAvaliacao(new Avaliacao("18/09/2026", 5, "Inesquecivel"));
 
         ArrayList<Restaurante> lista = new ArrayList<>();
         lista.add(burger);
-        lista.add(maison);
-        lista.add(terroir);
+        lista.add(murakami);
+        lista.add(evvai);
 
         for (Restaurante r : lista) {
             System.out.printf("%s - media: %.2f%n", r.getNome(), r.calcularMedia());
